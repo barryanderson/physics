91be9dc9cd788509a85cb9705ba04cca7e17c208
Barry Anderson <barryanderson@users.noreply.github.com>
2019-04-09 14:33:30 +0100

    Reworked converter

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -3,19 +3,51 @@ defmodule Converter do
     Float.ceil(value, 1)
   end
 
-  def to_km(velocity) do
-    velocity / 1000
+  def to_km(value) do
+    value / 1000
   end
 
-  def to_light_seconds({:miles, miles}) do
-    (miles * 5.36819e-6) |> round_down
+  def to_meters(value) do
+    value * 1000
   end
 
-  def to_light_seconds({:meters, meters}) do
-    (meters * 3.335638620368e-9) |> round_down
+  def to_light_seconds({:miles, miles} = value) do
+    (miles * 5.36819e-6) |> round_up
   end
 
-  def round_down(value) when is_float(value), do: trunc(value)
+  def to_light_seconds({:meters, meters} = value) do
+    (meters * 3.335638620368e-9) |> round_up
+  end
+
+  def to_light_seconds({:feet, feet} = value) do
+    (feet * 1.016702651488166404e-9) |> round_up
+  end
+
+  def to_light_seconds({:inches, inches} = value) do
+    (inches * 8.472522095734715723e-11) |> round_up
+  end
+
+  def round_up(value) when is_float(value), do: trunc(value)
+end
+
+defmodule ConverterTwo do
+  # ...
+
+  def to_light_seconds({unit, value}, precision: precision) do
+    case unit do
+      :miles -> from_miles(value)
+      :meters -> from_meters(value)
+      :feet -> from_feet(value)
+      :inches -> from_inches(value)
+    end
+    |> round_to(precision)
+  end
+
+  defp from_miles(value), do: value * 5.36819e-6
+  defp from_meters(value), do: value * 3.335638620368e-9
+  defp from_feet(value), do: value * 1.016702651488166404e-9
+  defp from_inches(value), do: value * 8.472522095734715723e-11
+  defp round_to(value, precision), do: Float.round(value, precision)
 end
 
 defmodule Physics.Rocketry do
